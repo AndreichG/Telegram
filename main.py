99ee525707dfa7a57dde585f3e1d997342641b1d
Lopatich <146587407+AndreichG@users.noreply.github.com>
2025-05-12 16:27:52 +0300

    code

--- main.py
+++ main.py
@@ -9,19 +9,12 @@ import sqlite3
 
 from cities import european_capitals, russian_big_cities, russian_small_cities
 
-
-async def start(update, context):
-    reply_keyboard = [['Ура']]
-    await update.message.reply_text('Начало бота',
-                                    reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-    return 0
-
 async def stop(update, context):
     await update.message.reply_text("Спасибо за игру")
     return ConversationHandler.END
 
-async def menu(update, context):
-    con = sqlite3.connect("db\stats")
+async def start(update, context):
+    con = sqlite3.connect("db\stats.db")
     cur = con.cursor()
     result1 = cur.execute(f"""SELECT * from stats WHERE username = \"{update.message.chat.username}\"""").fetchall()
     if not result1:
@@ -35,23 +28,22 @@ async def menu(update, context):
 
 
 async def fourth_response(update, context):
-    global mode
     if update.message.text == 'Играть':
-        reply_keyboard = [['Все города'], ["Столицы Европы"], ["Большие города России"], ["Маленькие города России"]]
+        reply_keyboard = [['Все города'], ["Столицы Европы"],  ["Россия"], ["Большие города России"], ["Маленькие города России"]]
         await update.message.reply_text('Выберите режим', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
         return 1
     if update.message.text == 'Посмотреть мои баллы':
-        con = sqlite3.connect("db\stats")
+        con = sqlite3.connect("db\stats.db")
         cur = con.cursor()
         result = cur.execute(
             f"""SELECT points from stats WHERE username = \"{update.message.chat.username}\"""").fetchall()
         con.close()
         await update.message.reply_text(f'Вот ваши баллы: {result[0][0]}')
-        await menu(update, context)
+        await start(update, context)
         return 4
     if update.message.text == 'Настройки сложности':
         reply_keyboard = [['1 (2 варианта ответа)'], ["2 (3 варианта ответа)"], ["3 (4 варианта ответа)"], ["4 (6 вариантов ответа)"], ["5 (8 вариантов ответа)"]]
-        con = sqlite3.connect("db\stats")
+        con = sqlite3.connect("db\stats.db")
         cur = con.cursor()
         result = cur.execute(
             f"""SELECT difficulty from stats WHERE username = \"{update.message.chat.username}\"""").fetchall()
@@ -61,7 +53,7 @@ async def fourth_response(update, context):
 
 async def fifth_response(update, context):
     diff = int(update.message.text[0])
-    con = sqlite3.connect("db\stats")
+    con = sqlite3.connect("db\stats.db")
     cur = con.cursor()
     result = cur.execute(
         f"""UPDATE stats
@@ -70,40 +62,49 @@ async def fifth_response(update, context):
     con.commit()
     con.close()
     await update.message.reply_text(f'Вам поставлена сложность {diff}')
-    await menu(update, context)
+    await start(update, context)
     return 4
 
 async def first_response(update, context):
-    global mode
+    con = sqlite3.connect("db\stats.db")
+    cur = con.cursor()
     if update.message.text == 'Все города':
         mode = 1
-        await cities_command(update, context)
     if update.message.text == 'Столицы Европы':
         mode = 2
-        await cities_command(update, context)
     if update.message.text == 'Россия':
         mode = 3
-        await cities_command(update, context)
     if update.message.text == 'Маленькие города России':
         mode = 4
-        await cities_command(update, context)
     if update.message.text == 'Большие города России':
         mode = 5
-        await cities_command(update, context)
+    print("я поменял режим", mode)
+    result = cur.execute(
+        f"""UPDATE stats
+            SET mode = {mode}
+            WHERE username = \"{update.message.chat.username}\"""").fetchall()
+    con.commit()
+    con.close()
+    await cities_command(update, context)
 
     return 2
 
 async def second_response(update, context):
-    global mode
     reply_keyboard = [['Играть дальше'], ['Конец игры'], ['В меню']]
-    con = sqlite3.connect("db\stats")
+    con = sqlite3.connect("db\stats.db")
     cur = con.cursor()
     result1 = cur.execute(
         f"""SELECT difficulty from stats WHERE username = \"{update.message.chat.username}\"""").fetchall()
     result2 = cur.execute(
         f"""SELECT points from stats WHERE username = \"{update.message.chat.username}\"""").fetchall()
+    result3 = cur.execute(
+        f"""SELECT city from stats WHERE username = \"{update.message.chat.username}\"""").fetchall()
+    result4 = cur.execute(
+        f"""SELECT mode from stats WHERE username = \"{update.message.chat.username}\"""").fetchall()
     diff = result1[0][0]
     points = result2[0][0]
+    city = result3[0][0]
+    mode = result4[0][0]
     mode_c = {1: 3,
               2: 2,
               3: 2.5,
@@ -121,7 +122,7 @@ async def second_response(update, context):
                 SET points = {points + dp}
                 WHERE username = \"{update.message.chat.username}\"""").fetchall()
         con.commit()
-        await update.message.reply_text(f'Вы угадали. Вам было выдано {round(dp, 2)} баллов\nТеперь у вас {round(points + dp, 2)} баллов')
+        await update.message.reply_text(f'Вы угадали. Вам было выдано {round(dp, 2)} баллов\nТеперь у вас {round(points + dp, 2)} баллов', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     else:
         dp = 1
         result = cur.execute(
@@ -129,9 +130,8 @@ async def second_response(update, context):
                 SET points = {points - dp}
                 WHERE username = \"{update.message.chat.username}\"""").fetchall()
         con.commit()
-        await update.message.reply_text(f"Вы не угадали. это был город {city}. У вас забрали {round(dp, 2)} баллов.\nТеперь у вас {round(points - dp, 2)} баллов")
+        await update.message.reply_text(f"Вы не угадали. это был город {city}. У вас забрали {round(dp, 2)} баллов.\nТеперь у вас {round(points - dp, 2)} баллов", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     con.close()
-    await update.message.reply_text('Что', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return 3
 
 async def third_response(update, context):
@@ -139,25 +139,36 @@ async def third_response(update, context):
         await cities_command(update, context)
         return 2
     elif update.message.text == 'В меню':
-        await menu(update, context)
+        await start(update, context)
         return 4
     elif update.message.text == 'Конец игры':
         await stop(update, context)
 
 async def cities_command(update, context):
-    global city
-    global mode
+
+    con = sqlite3.connect("db\stats.db")
+    cur = con.cursor()
+    result4 = cur.execute(
+        f"""SELECT mode from stats WHERE username = \"{update.message.chat.username}\"""").fetchall()
+    mode = result4[0][0]
+
     if mode == 1:
         cities = russian_big_cities + russian_small_cities + european_capitals
-    if mode == 2:
+    elif mode == 2:
         cities = european_capitals
-    if mode == 3:
+    elif mode == 3:
         cities = russian_big_cities + russian_small_cities
-    if mode == 4:
+    elif mode == 4:
         cities = russian_small_cities
-    if mode == 5:
+    elif mode == 5:
         cities = russian_big_cities
     city = random.choice(cities)
+
+    result = cur.execute(
+        f"""UPDATE stats
+            SET city = \"{city}\"
+            WHERE username = \"{update.message.chat.username}\"""").fetchall()
+    con.commit()
 
     text = city + random.choice(places)
     number = random.randint(1, 100)
@@ -165,7 +176,7 @@ async def cities_command(update, context):
         text = "Десантный мотобот"
         city = "Десантный мотобот 🎈"
     search_api_server = "https://search-maps.yandex.ru/v1/"
-    api_key = ""
+    api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
     search_params = {
         "apikey": api_key,
         "text": text,
@@ -183,7 +194,7 @@ async def cities_command(update, context):
     point = organization["geometry"]["coordinates"]
     org_point = f"{point[0]},{point[1]}"
     delta = "0.0017"
-    apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+    apikey = ""
 
     map_params = {
         "spn": ",".join([delta, delta]),
@@ -194,13 +205,11 @@ async def cities_command(update, context):
     map_api_server = "https://static-maps.yandex.ru/v1"
     response = requests.get(map_api_server, params=map_params)
 
-    con = sqlite3.connect("db\stats")
+    con = sqlite3.connect("db\stats.db")
     cur = con.cursor()
     result = cur.execute(
         f"""SELECT difficulty from stats WHERE username = \"{update.message.chat.username}\"""").fetchall()
-    con.close()
     diff = result[0][0]
-
     if diff == 1:
         reply_keyboard = [[city], [random.choice(cities)]]
     if diff == 2:
@@ -218,20 +227,21 @@ async def cities_command(update, context):
         response.content,
         caption="Угадайте город на карте", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     )
+    con.close()
 
 places = [
         "Ресторан",  # Заведение для полноценного питания
         "Магазин",  # Общий термин для магазинов различного профиля
         "Больница",  # Учреждение для оказания медицинской помощи
         "Гостиница"
+        "Почта"
+        "Полиция"
+        "Администрация"
     ]
-city = ''
-mode = -1
 application = Application.builder().token('').build()
 conv_handler = ConversationHandler(
     entry_points=[CommandHandler('start', start)],
     states={
-        0: [MessageHandler(filters.TEXT & ~filters.COMMAND, menu)],
         1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
         2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)],
         3: [MessageHandler(filters.TEXT & ~filters.COMMAND, third_response)],
